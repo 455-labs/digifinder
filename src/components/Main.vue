edd76dcb4cfdf3dbcc82108bd7055b12b58fd10f
VanHansen <samu.kouhia@gmail.com>
2025-11-25 14:34:03 +0200

    feat: Add :id path to active digimon

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -163,6 +163,7 @@ export default {
       <!-- Digimon image card -->
       <DisplayDigimon
         :img="activeDigimon?.images?.[0]?.href"
+        :id="activeDigimon?.id"
         @prev="showPreviousDigimon"
         @next="showNextDigimon"
       />
