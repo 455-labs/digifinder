79d3cbb106efa3e7212dd0dcb0f1c0a7d9bd5892
VanHansen <samu.kouhia@gmail.com>
2025-12-06 15:27:28 +0200

    docs: Add comments to main.js script

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
+// Entry point of the Vue application. This file initializes Vue, Vuetify,
+// and global styles, then mounts the root component (App.vue).
 import { createApp } from 'vue'
 import App from './App.vue'
 import './assets/styles.css'
 
+// Importing Vuetify's styles and core library components.
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+// Material Design Icons used by Vuetify.
 import '@mdi/font/css/materialdesignicons.css'
 
+// Creating the Vuetify instance with all components and directives registered.
 const vuetify = createVuetify({
   components,
   directives,
 })
 
+// Creating and mounting the Vue application instance.
+// Vuetify is registered globally via .use().
 createApp(App).use(vuetify).mount('#app')
